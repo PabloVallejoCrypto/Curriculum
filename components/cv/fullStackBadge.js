7c4cfc98ebfda4ed5b1a37328f422175e032d5b2
Pablo Vallejo <pablovallejo.gon@gmail.com>
2021-10-06 16:46:26 +0200

    V1

--- components/cv/fullStackBadge.js
+++ components/cv/fullStackBadge.js
@@ -2,7 +2,8 @@ import Image from 'next/image'
 import Link from 'next/link'
 import reactBadge from '../../public/reactBadge.png'
 
-const rowStyle = {padding: "1%", margin: "2%", minHeight: "100vh", alignItems: "center"};
+const rowStyle = {padding: "1%", margin: "3%", minHeight: "100vh"};
+const marginStyle = {margin: "3%"};
 const textLeft = {textAlign: "left"};
 const textRight = {textAlign: "right"};
 
@@ -17,23 +18,36 @@ export default function DescriptionRow() {
 					alt="React Badge"
 					/>
 			</div>
-			<div className='col-lg-4 mx-auto text-center'>
+			<div className='col-lg-4 mx-auto text-center' style={marginStyle}>
 				<div style={textLeft}>
 					<h1 className='display-4'>
 						⚛️ React
 					</h1>
 					<h1 className='lead'>  JSX  ·  BABEL  </h1>
-					<p> Much react and more react, with something about react  </p>
+					<p> JSX syntax for component model based. Components life flow, responsive design, libraries, frameworks and others implementations.  </p>
 				</div>
 				<div style={textRight}>
 					<h1 className='display-4'>
 						Next
 					</h1>
 					<h1 className='lead'>  SERVER-SIDE RENDERING  </h1>
-					<p> Much next and more next, with something about next  </p>
+					<p>  Next generation for server. Secure server-side REST-APIs, dynamic fetch and routing, Redux store based and much more.  </p>
 				</div>
-				<h1 className='display-6'>  Redux  </h1>
-				<h1 className='lead'> That site uses redux  </h1>
+				<div style={textLeft}>
+					<h1 className='display-4'>
+						HTML5
+					</h1>
+					<h1 className='lead'>   ES6 Syntax  </h1>
+					<p>  +5 years of HTML5, CSS3 and JS development. New syntaxts methods. Responsive UIs. Secure client side calls. React, Socket.io, Web3, BabylonJS and more.  </p>
+				</div>
+				<div style={textRight}>
+					<h1 className='display-4'>
+						ExpressJS
+					</h1>
+					<h1 className='lead'>  +2 years of Back-End with ExpressJS. REST-APIs, Helmet, CORS, MongoDB, dynamic routing and more.  </h1>
+				</div>
+				<h1 className='display-6'>  Redux & Web3  </h1>
+				<h1 className='lead'> IMPLEMENTED  </h1>
 				<Link href='https://github.com/PabloVallejoCrypto/Curriculum'>
 					<button className='btn mx-auto mb-3 magicButton1'> This site Repository  </button>
 				</Link>
