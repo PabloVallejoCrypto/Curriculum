b5ed323e6f02182c50d6fc8f2982edc361f6e770
Pablo Vallejo <pablovallejo.gon@gmail.com>
2021-10-06 17:51:30 +0200

    V1

--- components/crypto/initiMetamask.js
+++ components/crypto/initiMetamask.js
@@ -52,10 +52,12 @@ export default function MetamaskConnection() {
 
 	return (
 		<div className={styles.container} style={{backgroundColor: "#333"}}>
-			<button className='btn mb-3 mx-auto magicButton cuadrado' onClick={connectMetamask}> CONNECT WALLET  </button>
-			<Link href='/apps'>
-				<button className='btn mb-3 mx-auto magicButton cuadrado'> RETURN </button>
-			</Link>
+			<div className='col-lg-6 mx-auto'>
+				<button className='btn mb-3 mx-auto magicButton cuadrado' onClick={connectMetamask}> CONNECT WALLET  </button>
+				<Link href='/apps'>
+					<button className='btn mb-3 mx-auto magicButton cuadrado'> RETURN </button>
+				</Link>
+			</div>
 		</div>
 	)
 }
