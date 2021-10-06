ea9d306ad0cb21a8ceec53cc158af8a9113a4557
Pablo Vallejo <pablovallejo.gon@gmail.com>
2021-10-06 16:48:38 +0200

    V1

--- components/cv/skills.js
+++ components/cv/skills.js
@@ -22,7 +22,7 @@ export default function Skills() {
 					<span className='btn mb-3 badgeBlack'><h1 className='lead'>  C  </h1></span> 
 					<span className='btn mb-3 badgeBlack'><h1 className='lead'>  HTML5  </h1></span>
 					<span className='btn mb-3 badgeBlack'><h1 className='lead'>  JavaScript(ES6)  </h1></span>
-					<span className='btn mb-3 badgeBlack'><h1 className='lead'>  CSS  </h1></span>
+					<span className='btn mb-3 badgeBlack'><h1 className='lead'>  CSS3  </h1></span>
 					<span className='btn mb-3 badgeBlack'><h1 className='lead'>  Solidity  </h1></span>
 					<span className='btn mb-3 badgeBlack'><h1 className='lead'>  Python  </h1></span>
 					<span className='btn mb-3 badgeBlack'><h1 className='lead'>  React(JSX-Babel)  </h1></span>
@@ -31,19 +31,20 @@ export default function Skills() {
 			<div className='col-lg-5 cardStyle mx-auto' style={cardStyle}>
 				<h1 className='display-6'>  SKILLS  </h1>
 				<div className='col-lg-12 mx-auto lead' style={innerCardStyle}>
-					<span className='btn mb-3 badgeBlack'><h1 className='lead'>  NextJs  </h1></span>
-					<span className='btn mb-3 badgeBlack'><h1 className='lead'>  React  </h1></span>
+					<span className='btn mb-3 badgeBlack'><h1 className='lead'>  NextJS  </h1></span>
+					<span className='btn mb-3 badgeBlack'><h1 className='lead'>  ReactJS  </h1></span>
 					<span className='btn mb-3 badgeBlack'><h1 className='lead'>  Redux  </h1></span>
 					<span className='btn mb-3 badgeBlack'><h1 className='lead'>  Scrum(Agile)  </h1></span>
-					<span className='btn mb-3 badgeBlack'><h1 className='lead'>  NodeJs  </h1></span>
-					<span className='btn mb-3 badgeBlack'><h1 className='lead'>  Express  </h1></span>
+					<span className='btn mb-3 badgeBlack'><h1 className='lead'>  NodeJS  </h1></span>
+					<span className='btn mb-3 badgeBlack'><h1 className='lead'>  ExpressJS  </h1></span>
 					<span className='btn mb-3 badgeBlack'><h1 className='lead'>  DeepAI  </h1></span>
-					<span className='btn mb-3 badgeBlack'><h1 className='lead'>  BabylonJs  </h1></span>
+					<span className='btn mb-3 badgeBlack'><h1 className='lead'>  BabylonJS  </h1></span>
 					<span className='btn mb-3 badgeBlack'><h1 className='lead'>  Socket.io  </h1></span>
-					<span className='btn mb-3 badgeBlack'><h1 className='lead'>  Ethereum(EVM) & Binance  </h1></span>
+					<span className='btn mb-3 badgeBlack'><h1 className='lead'>  Ethereum(EVM)  </h1></span>
+					<span className='btn mb-3 badgeBlack'><h1 className='lead'>  Binance Smart Chain  </h1></span>
 					<span className='btn mb-3 badgeBlack'><h1 className='lead'>  Truffle & Ganache  </h1></span>
 					<span className='btn mb-3 badgeBlack'><h1 className='lead'>  WEB3  </h1></span>
-					<span className='btn mb-3 badgeBlack'><h1 className='lead'>  BigNumberJs  </h1></span>
+					<span className='btn mb-3 badgeBlack'><h1 className='lead'>  BigNumberJS  </h1></span>
 				</div>   
 			</div>	
 		</div>
