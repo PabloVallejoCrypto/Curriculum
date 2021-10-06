1e2bec0426d7ddea8aef1194198120783201ec5a
Pablo Vallejo <pablovallejo.gon@gmail.com>
2021-10-06 16:47:06 +0200

    V1

--- components/cv/name.js
+++ components/cv/name.js
@@ -5,15 +5,16 @@ const titleStyle = {padding: "3%", margin: "2%"};
 
 export default function Name() {
 	return (	
-	<div className='col-lg-8 mx-auto' style={titleStyle}>
+	<div className='col-lg-6 mx-auto' style={titleStyle}>
 		<h1 className='display-2'>
 			Pablo Vallejo
 		</h1>
 		<h1 className='display-6'>	Full Stack Developer	</h1>
 		<h1 className='lead'>   BLOCKCHAIN  ·  WEB   </h1>
+		<p>  BARCELONA, 1998  </p>
 		<div className='row mx-auto'>
 			<Link href='#mainSkills'><button className='btn mx-auto magicButton1 cuadrado'>  NEXT  </button></Link>
-			<Link href='#contact'><button className='btn mx-auto magicButton1 cuadrado'>  CONTACT  </button></Link>
+			<Link href='#contact'><button className='btn mx-auto magicButton1 cuadrado'> CONTACT  </button></Link>
 		</div>			
 	</div>			
 	)
