417ff7a8f30b060ae37141922e8491edf717584e
Pablo Vallejo <pablovallejo.gon@gmail.com>
2021-10-06 16:45:37 +0200

    V1

--- components/cv/descriptionRow.js
+++ components/cv/descriptionRow.js
@@ -1,6 +1,5 @@
 import Image from 'next/image'
 import Link from 'next/link'
-import styles from '../../styles/Home.module.css'
 
 const rowStyle = {padding: "1%", margin: "2%", minHeight: "100vh", alignItems: "center", animation: "keepDeath 0s ease 0s forwards"};
 const someMargin = {margin: "2%"};
@@ -13,29 +12,18 @@ export default function DescriptionRow() {
 		<div className="row mx-auto text-center" id="mainSkills" style={rowStyle}>
 			<div className='col-lg-3 mx-auto cardStyle' style={someMargin}>
 				<Image
-				    src={'https://img.icons8.com/fluency/240/000000/developer-mode.png'}
-				    width="65px"
-				    height="65px"
-					alt="Developer Phone"
-					/>
-					<h1 className='display-6'>
-						Full Stack
-					</h1>
-					<h1 className='lead' style={textLeft}>  + 5 years of personal Nodejs projects with ES6 syntaxt. Back-end REST-API made with Express JS.   </h1>
-					<Link href='#fullStack'><button className='btn mx-auto magicButton1' style={buttonStyle}>  DISCOVER  </button></Link>
-			</div>
-			<div className='col-lg-3 mx-auto cardStyle' style={someMargin}>
-				<Image
 				    src={'https://img.icons8.com/officel/480/000000/react.png'}
 				    width="65px"
 				    height="65px"
 					alt="Reactjs Logo"
 					/>
 					<h1 className='display-6'>
-						React  &  NextJS
+						Full Stack
 					</h1>
-					<h1 className='lead' style={textLeft}>  Next server generation with sever-side rendering. Faster and more secure apps. JSX - Babel syntax and Redux implementation.  </h1>
-					<Link href='#fullStack'><button className='btn mx-auto magicButton1' style={buttonStyle}>  DISCOVER  </button></Link>
+					<h1 className='lead' style={textLeft}>  + 5 years of personal NodeJS(ES6 syntaxt) projects.  </h1>
+					<h1 className='lead' style={textLeft}>  NextJS sever-side rendering. JSX - Babel syntax, Redux and REST-API implementation.  </h1>
+					<h1 className='lead' style={textLeft}>  Back-end ExpressJS.  </h1>
+					<Link href='#fullStack'><button className='btn mx-auto magicButton1' style={buttonStyle}>  MORE  </button></Link>
 			</div>
 			<div className='col-lg-3 mx-auto cardStyle' style={someMargin}>
 				<Image
@@ -47,8 +35,10 @@ export default function DescriptionRow() {
 					<h1 className='display-6'>
 						SOLIDITY
 					</h1>
-					<h1 className='lead' style={textLeft}>  + 2 years developing Smart Contracts, creating and implementing cryptographic protocols. WebSite implementation.  </h1>
-					<Link href='#solidity'><button className='btn mx-auto magicButton1' style={buttonStyle}>  DISCOVER  </button></Link>
+					<h1 className='lead' style={textLeft}>  + 2 years BlockChain experience, developing Smart Contracts, creating and implementing in-chain protocols.  </h1>
+					<h1 className='lead' style={textLeft}>  Web implementation, via Web3, of own contracts, exchanges and other protocols.  </h1>
+					<p className='' style={textLeft}>  On GitHub: Random Generator, Basic Democratic System, Chaotic Token and more.  </p>
+					<Link href='#solidity'><button className='btn mx-auto magicButton1' style={buttonStyle}>  MORE  </button></Link>
 			</div>
 			
 		</div>
