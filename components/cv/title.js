75009a61775be64c09273042fd5feb269a8b3d2b
Pablo Vallejo <pablovallejo.gon@gmail.com>
2021-10-06 16:49:21 +0200

    V1

--- components/cv/title.js
+++ components/cv/title.js
@@ -1,24 +1,11 @@
-import Image from 'next/image'
-import MyFace from '../../public/profilePhoto.jpg'
 import Name from './name.js'
-
-const titleStyle = {padding: "1%", margin: "2%", minHeight: "100vh", justifyContent: "center", animation: "coolStart 2s ease-in-out"};
-const containerStyle = {maxHeight: "35px"};
+import ProfilePhoto from './profilePhoto.js'
+const titleStyle = {padding: "1%", margin: "2%", height: "100vh", justifyContent: "center", animation: "coolStart 2s ease-in-out"};
 
 export default function Title() {
 	return (
 		<div className='row mx-auto text-center' style={titleStyle}>
-			<div className='col-lg-4 mx-auto'>
-				<Image
-				    src={MyFace}
-				    style={containerStyle}
-				    className='redondeado'
-				    layout="responsive"
-				    width="220px"
-				    height="230px"
-					alt="Pablo Vallejo Face"
-					/>
-			</div>
+			<ProfilePhoto   />
 			<Name  />	
 				
 		</div>
